74b0e423d347157a7f71abec87aa1c618a219ec5
Daniel <dhuerta1408@gmail.com>
2019-10-30 19:00:33 -0600

    create main handlebars, et up server.js

---
+++ controllers/scrapeController.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+let exphbs = require("express-handlebars");
+
+var axios = require("axios");
+
+var cheerio = require("cheerio");
+
+var db = require("./models");
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+
+var morgan = require("morgan");
+
+var mongoose = require("mongoose");
+
+let exphbs = require("express-handlebars");
+
+var axios = require("axios");
+
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 8080;
+
+var app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+require("./controllers/scrapeController.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
